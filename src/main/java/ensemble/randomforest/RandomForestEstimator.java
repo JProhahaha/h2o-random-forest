c8abe4de2ca1f9a63c80c8884f265140f06b5a66
json-p <majpro007@gmail.com>
2016-07-23 15:58:21 -0700

    Added argument parsing call and JUnit test.

--- src/main/java/ensemble/randomforest/RandomForestEstimator.java
+++ src/main/java/ensemble/randomforest/RandomForestEstimator.java
@@ -1,5 +1,19 @@
 package ensemble.randomforest;
 
+/**
+ * Estimator class for Random Forest. This class instantiates an instance of
+ * <code>h2o.ai</code> and builds a Random Forest model to save as a Java POJO.
+ * 
+ * @author JP
+ */
 public class RandomForestEstimator {
 
+	public static void main(String[] args) {
+		RFArguments cliArgs = RFUtils.parseRFArguments(args);
+		// Exit program if help was used
+		if (cliArgs.help)
+			return;
+
+		// TODO Instantiate H2O instance
+	}
 }
--- src/test/java/ensemble/randomforest/RandomForestEstimatorTest.java
+++ src/test/java/ensemble/randomforest/RandomForestEstimatorTest.java
@@ -1,5 +1,18 @@
 package ensemble.randomforest;
 
-public class RandomForestEstimatorTest {
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+public class RandomForestEstimatorTest {
+	public static String[] args;
+
+	@BeforeClass
+	public static void setup(){
+		args = new String[] { "-h" };
+	}
+	
+	@Test
+	public void testMain() {
+		RandomForestEstimator.main(args);
+	}
 }
