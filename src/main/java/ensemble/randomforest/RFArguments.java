68d76ef4fd46a9e074b5412cebea3a370fb21a23
json-p <majpro007@gmail.com>
2016-07-23 15:53:08 -0700

    Added `RFARguments` class for parsing the command line arguments with `JCommnander`

---
+++ src/main/java/ensemble/randomforest/RFArguments.java
@@ -0,0 +1,28 @@
+package ensemble.randomforest;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * JCommander command line arguments class for Random Forest Estimator.
+ * 
+ * @author JP
+ */
+public class RFArguments {
+	@Parameter(names = { "-h", "--help" }, description = "Displays Help Usage", help = true)
+	public boolean help;
+
+	@Parameter(names = { "-i", "--input" }, description = "Path to date file for training", required = true)
+	public String inputFilePath;
+
+	@Parameter(names = { "-o", "--output" }, description = "Path to output directory. (Default root)")
+	public String outputDir = ".";
+	
+	@Parameter(names = "--h2o-help", description = "Displays help usage for h2o specific parameters.", help = true)
+	public boolean h2oHelp;
+	
+	@Parameter(names = "--h2o-params", description = "h2o specific parameters for instantiation in a \"quoted string\".")
+	public String h2oParams = "";
+	
+	@Parameter(names = "--h2o-quiet", description = "Used to avoid printing h2o logging to the console. (Default false)")
+	public boolean h2oQuiet = false;
+}
